3b03dd8206bf441831a0e72d9b65369e2c8464d0
sam-a-levy <salevy@ethz.ch>
2020-10-21 21:58:03 +0200

    updated code task 5

--- task-5-kings.R
+++ task-5-kings.R
@@ -10,11 +10,12 @@ names_of_students <- paste(rep("student",25),1:25,sep="-")
 
 #determine number of groups required
 no_of_groups <- ceiling(length(names_of_students)/length(houses))
+group_size <- length(houses)
 
 #create data frame where students randomly assigned house & character
 data.frame("characters" = rep(characters[1:no_of_groups],no_of_groups),
-           "houses" = c(rep(houses[1],no_of_groups),rep(houses[2],no_of_groups),
-                        rep(houses[3],no_of_groups),rep(houses[4],no_of_groups),rep(houses[5],no_of_groups)),
+           "houses" = c(rep(houses[1],group_size),rep(houses[2],group_size),
+                        rep(houses[3],group_size),rep(houses[4],group_size),rep(houses[5],group_size)),
            "names_of_students" = sample(names_of_students)) %>%
   group_by(characters) %>% arrange(.by_group=T)
   
